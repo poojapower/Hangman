491325ac9fda0a6d05af66096ccde5500b6266fe
poojapower <54731691+poojapower@users.noreply.github.com>
2020-07-20 14:24:08 -0700

    Updates

---
+++ precache-manifest.25342df7d08d02416dd6fec3b007b7bd.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "adfb498daf5dda49c3066cf7141a4efc",
+    "url": "/Hangman/index.html"
+  },
+  {
+    "revision": "8417ca8a51b3d9eacc10",
+    "url": "/Hangman/static/css/main.341a48cd.chunk.css"
+  },
+  {
+    "revision": "d4b90e2067b45528a8d4",
+    "url": "/Hangman/static/js/2.90c0c040.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/Hangman/static/js/2.90c0c040.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "8417ca8a51b3d9eacc10",
+    "url": "/Hangman/static/js/main.65d07fcc.chunk.js"
+  },
+  {
+    "revision": "8a93cdc09629d2b7005c",
+    "url": "/Hangman/static/js/runtime-main.707d265e.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/Hangman/static/media/logo.5d5d9eef.svg"
+  }
+]);
